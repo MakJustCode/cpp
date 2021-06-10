a7ccc3c54d1c350e93678d2b51a558630bf0c7ff
Makenson Noel <Makenson.noel@yahoo.com>
2021-06-10 18:44:29 -0400

    Add files via upload

---
+++ Practicing Class/Practicing Class/ClassPractice.cpp
@@ -0,0 +1,5 @@
+#include "ClassPractice.h"
+
+void Pick::Rock()
+{
+}
---
+++ Practicing Class/Practicing Class/ClassPractice.h
@@ -0,0 +1,24 @@
+#ifndef ClassPractice_h
+#define ClassPractice_h
+#include <iostream>
+
+
+class Pick
+{
+	
+	
+	public:
+		int age;
+		int height;
+		void Rock();
+		void Scissors(); 
+		void Paper(); 
+		
+		
+	private:
+		int passWord; 	
+};
+
+
+#endif
+
