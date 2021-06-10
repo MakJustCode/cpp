ec35acf4713a397c059d126fb141326471000db3
Makenson Noel <Makenson.noel@yahoo.com>
2021-06-10 18:40:47 -0400

    Add files via upload

---
+++ E$0/E$0/Contribute.cpp
@@ -0,0 +1,4 @@
+#include "Contribute.h"
+
+
+
---
+++ E$0/E$0/Contribute.h
@@ -0,0 +1,30 @@
+#ifndef Contribute_h
+#define Contribute_h
+#include <iostream>
+#include <string>
+
+class Members
+{
+	public:
+		int memberLimit, contributed, moneyPot, result, E$O;
+		long int deadline; 
+		std::string memberName; 
+		
+	
+
+
+	private:
+	const int lateFee = 5;
+	
+
+
+
+
+
+
+};
+
+
+
+
+#endif
