299631d85cd701fe97628baa7c1f77aa69663b71
Makenson Noel <Makenson.noel@yahoo.com>
2021-06-10 18:42:24 -0400

    Add files via upload

---
+++ Infinite Realities/Infinite Realities/infinite Realitiess.cpp
@@ -0,0 +1,37 @@
+// A Different World, Infinite Realities 
+#include <iostream>
+#include <string>
+
+int main()
+{
+	system("Color 3e");
+	std::string answer;
+	
+	std::cout << "Hello" << std::endl;
+	
+	while (answer=="Y" || answer=="y")
+	{
+	
+	
+	//Door 1 Good Life
+	
+	
+	
+	
+	//Door 2 Bad Life
+	
+	
+	
+	}
+	
+	
+	system("Pause");
+	
+	return 0;
+}
+
+
+
+
+
+
