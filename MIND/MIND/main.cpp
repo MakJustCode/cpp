0a78f4e6152a5b1f34c5fea4071979e85f3c2d84
Makenson Noel <Makenson.noel@yahoo.com>
2021-06-10 18:43:36 -0400

    Add files via upload

---
+++ MIND/MIND/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+	//Mind 
+	//Programmed by Makenson Noel 
+	//Date 7/9/2020
+	// Time (Unknown xD ) 
+
+	std::string name;
+	char answer; 
+
+	std::cout << "Enter Name: ";
+	std::cin >> name;
+
+
+	std::cout << "Hello " << name << " welcome to my mind"; 
+
+
+	//Decisions 
+	std::cout << "Does free will exist ?"; 
+	std::cin >> answer; 
+
+
+	return 0; 
+}
