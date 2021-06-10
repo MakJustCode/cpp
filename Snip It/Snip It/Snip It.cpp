2cd7f3b10233a16043aba4e1b9ef3ee48e7ae27b
Makenson Noel <Makenson.noel@yahoo.com>
2021-06-10 18:47:44 -0400

    Add files via upload

---
+++ Snip It/Snip It/Snip It.cpp
@@ -0,0 +1,33 @@
+//Snip It 
+
+
+// This program will take images and show you where you can shop for them 
+
+#include <iostream>
+
+using namespace std; 
+
+int main()
+{
+	cout << "-----------Welcome to snip it------------- " << endl << endl; 
+	
+	cout << "Are you ready ? " << endl << endl ; 
+	
+	cout << "Snap a Photo " << endl; 
+	
+	//system("cls");
+	
+	cout << "Searching . . ." << endl;
+	
+	//system("cls");
+	
+	cout << "This appears to be at " << endl;
+	
+	
+	system("pause");
+	
+	
+	
+	
+	return 0;
+}
