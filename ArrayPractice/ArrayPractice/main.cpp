6d3dc9d17beb6e3a5b3ca978004fdb10cc6577ec
Makenson Noel <Makenson.noel@yahoo.com>
2021-06-10 18:38:35 -0400

    Add files via upload

---
+++ ArrayPractice/ArrayPractice/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	int mak[100]; 
+	int shares; 
+
+	std::cout << "Enter amount of shares: "; 
+	std::cin >> shares; 
+
+	int YOO = shares; 
+
+	
+	std::cout << "Shares : " << YOO--; 
+	
+
+
+
+	return 0; 
+}
