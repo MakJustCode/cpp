7e6d0b03a205efd9401f9792533a9ffe28f678f0
Makenson Noel <Makenson.noel@yahoo.com>
2021-06-10 18:39:12 -0400

    Add files via upload

---
+++ Binary digits/Binary digits/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+
+int main()
+{
+	//There is a pattern to binary digits 
+	std::cout << "  0  " << "0000  " << std::endl;
+	std::cout << "  1  " << "0001  " << std::endl;
+	std::cout << "  2  " << "0010  " << std::endl;
+	std::cout << "  3  " << "0011  " << std::endl;
+	std::cout << "  4  " << "0100  " << std::endl;
+	std::cout << "  5  " << "0101  " << std::endl;
+	std::cout << "  6  " << "0110  " << std::endl;
+	std::cout << "  7  " << "0111  " << std::endl;
+	std::cout << "  8  " << "1000  " << std::endl;
+	std::cout << "  9  " << "1001  " << std::endl;
+	std::cout << " 10  " << "1010  " << " A " << std::endl;
+	std::cout << " 11  " << "1011  " << " B " << std::endl;
+	std::cout << " 12  " << "1100  " << " C " << std::endl;
+	std::cout << " 13  " << "1101  " << " D " << std::endl;
+	std::cout << " 14  " << "1110  " << " E " << std::endl;
+	std::cout << " 15  " << "1111  " << " F " << std::endl;
+
+
+
+
+	system("Pause");
+
+	return 0;
+}
