6029cd23e911c5e2a93bcd6cc01b3e677de3803d
Makenson Noel <Makenson.noel@yahoo.com>
2021-06-10 18:42:48 -0400

    Add files via upload

---
+++ Landscaping/Landscaping/Header.h
@@ -0,0 +1,23 @@
+#ifndef Header_h
+#define Header_h
+#include <iostream>
+
+class setAppointment
+{
+	public:
+	setAppointment();
+};
+
+class Months
+{
+	public:
+	Months();
+	
+};
+
+
+
+
+
+
+#endif 
---
+++ Landscaping/Landscaping/Main.cpp
@@ -0,0 +1,10 @@
+#include "Header.h"
+
+int main()
+{
+	
+	 
+
+	system("Pause"); 
+	return 0; 
+}
